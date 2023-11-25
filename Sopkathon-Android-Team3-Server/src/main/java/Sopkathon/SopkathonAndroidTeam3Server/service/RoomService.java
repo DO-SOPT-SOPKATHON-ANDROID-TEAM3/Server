26f437af6bcf0c26084d37d77f5e4cca0db59644
gardening-y <60008786@naver.com>
2023-11-26 03:19:55 +0900

    [FEAT] RoomService enter 추가

--- Sopkathon-Android-Team3-Server/src/main/java/Sopkathon/SopkathonAndroidTeam3Server/service/RoomService.java
+++ Sopkathon-Android-Team3-Server/src/main/java/Sopkathon/SopkathonAndroidTeam3Server/service/RoomService.java
@@ -3,6 +3,7 @@ package Sopkathon.SopkathonAndroidTeam3Server.service;
 import Sopkathon.SopkathonAndroidTeam3Server.domain.Room;
 import Sopkathon.SopkathonAndroidTeam3Server.domain.User;
 import Sopkathon.SopkathonAndroidTeam3Server.dto.request.UserCreateRequest;
+import Sopkathon.SopkathonAndroidTeam3Server.dto.request.UserEnterRequest;
 import Sopkathon.SopkathonAndroidTeam3Server.dto.response.RoomCreateResponse;
 import Sopkathon.SopkathonAndroidTeam3Server.repository.RoomRepository;
 import Sopkathon.SopkathonAndroidTeam3Server.repository.UserRepository;
@@ -20,7 +21,7 @@ public class RoomService {
     public RoomCreateResponse createRoom(UserCreateRequest request) {
         Room room = roomRepository.save(
                 Room.builder()
-                        .code("Gooday")
+                        .code("GOODAY")
                         .build()
         );
 
@@ -32,5 +33,17 @@ public class RoomService {
         );
 
         return RoomCreateResponse.of(room);
+    }
+
+    public void enterRoom(UserEnterRequest request) {
+        Room room = roomRepository.findByCode(request.code()).orElseThrow(() -> new IllegalArgumentException("잘못된 코드"));
+
+        User user = userRepository.save(
+                User.builder()
+                        .name(request.name())
+                        .room(room)
+                        .build()
+        );
+
     }
 }
