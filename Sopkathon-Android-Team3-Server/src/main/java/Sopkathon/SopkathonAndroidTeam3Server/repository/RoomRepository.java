d0b455104b662d9385063ab1256cebc40aafa09e
gardening-y <60008786@naver.com>
2023-11-26 03:18:24 +0900

    [FEAT] RoomRepository 수정

--- Sopkathon-Android-Team3-Server/src/main/java/Sopkathon/SopkathonAndroidTeam3Server/repository/RoomRepository.java
+++ Sopkathon-Android-Team3-Server/src/main/java/Sopkathon/SopkathonAndroidTeam3Server/repository/RoomRepository.java
@@ -3,7 +3,9 @@ package Sopkathon.SopkathonAndroidTeam3Server.repository;
 import Sopkathon.SopkathonAndroidTeam3Server.domain.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoomRepository extends JpaRepository<Room, Long> {
+import java.util.Optional;
 
+public interface RoomRepository extends JpaRepository<Room, Long> {
+    Optional<Room> findByCode(String code);
 
 }
