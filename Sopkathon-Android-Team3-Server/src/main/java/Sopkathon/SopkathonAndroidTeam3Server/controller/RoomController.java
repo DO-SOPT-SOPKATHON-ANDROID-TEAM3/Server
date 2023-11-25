a49033ea7bade307727bae4c5fb6083809a5f5ac
gardening-y <60008786@naver.com>
2023-11-26 03:17:47 +0900

    [FEAT] RoomEnter Controller 생성

--- Sopkathon-Android-Team3-Server/src/main/java/Sopkathon/SopkathonAndroidTeam3Server/controller/RoomController.java
+++ Sopkathon-Android-Team3-Server/src/main/java/Sopkathon/SopkathonAndroidTeam3Server/controller/RoomController.java
@@ -2,10 +2,12 @@ package Sopkathon.SopkathonAndroidTeam3Server.controller;
 
 
 import Sopkathon.SopkathonAndroidTeam3Server.dto.request.UserCreateRequest;
+import Sopkathon.SopkathonAndroidTeam3Server.dto.request.UserEnterRequest;
 import Sopkathon.SopkathonAndroidTeam3Server.dto.response.RoomCreateResponse;
 import Sopkathon.SopkathonAndroidTeam3Server.service.RoomService;
 import Sopkathon.SopkathonAndroidTeam3Server.util.BaseApiException;
 import Sopkathon.SopkathonAndroidTeam3Server.util.BaseApiResponse;
+import Sopkathon.SopkathonAndroidTeam3Server.util.BaseApiResponseNonData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,8 +22,14 @@ public class RoomController {
     private final RoomService roomService;
 
     @PostMapping("/create")
-    public BaseApiResponse<RoomCreateResponse> creasteRoom(@RequestBody UserCreateRequest request) {
+    public BaseApiResponse<RoomCreateResponse> createRoom(@RequestBody UserCreateRequest request) {
         return new BaseApiResponse<>("방 생성 완료", roomService.createRoom(request));
+    }
+
+    @PostMapping("/enter")
+    public BaseApiResponseNonData enter (@RequestBody UserEnterRequest request) {
+        roomService.enterRoom(request);
+        return new BaseApiResponseNonData("방 입장하기 완료");
     }
 
 }
